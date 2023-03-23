af7e072f84a9baa57bb590c6f332aeaca9d91dd1
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-03-23 14:37:04 -0300

    Praticando

---
+++ exercicios/ex013/script.js
@@ -0,0 +1,17 @@
+var msg = window.document.getElementById('acao') 
+
+function clicou1() {   
+    msg.innerHTML += '<p>Você clicou no 1° botão</p>'
+} 
+
+function clicou2() { 
+    msg.innerHTML += '<p>Você clicou no 2° botão</p>'
+}
+
+function clicou3() {
+    msg.innerHTML += '<p>Você clicou no 3° botão</p>'
+}
+
+function clicou4() {
+    msg.innerHTML += '<p>Você clicou no 4° botão</p>'
+}
