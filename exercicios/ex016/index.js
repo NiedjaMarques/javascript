de75bb9acb17d38666dd36c31ca2d08190c62cf5
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-03-27 16:47:02 -0300

    praticando

--- exercicios/ex016/index.js
+++
@@ -1,7 +0,0 @@
-function calcular(){
-    var txtv = window.document.getElementById('txtvel')
-    var res = window.document.getElementById('res')
-    var vel = Number(txtv.value)
-    res.innerHTML = `<p>Sua velocidade atual é de <strong>${vel}Km/h</strong></p>`   
-}
-
