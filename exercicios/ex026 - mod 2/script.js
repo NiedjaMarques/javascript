1f51ea67d8e1a4f9097887e68c4af3d1d93c6ba1
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-04-03 10:14:35 -0300

    praticando

---
+++ exercicios/ex026 - mod 2/script.js
@@ -0,0 +1,25 @@
+/*Variáveis consideradas GLOBAIS */
+var res = document.getElementById('result')
+var computador = 0
+var jogador = 0
+
+function sortear() { //esta função esta dentro do body
+    var min = 1 
+    var max = 100
+    var dif = max - min
+    var aleatorio = Math.random()
+    computador = min + Math.trunc(dif * aleatorio)
+}
+
+function jogar() {
+    var jogador = Number(window.prompt('Qual é o seu palpite?'))
+
+    if (jogador < computador) {
+        res.innerHTML += `<p>Você falou ${jogador}. Meu número é <strong>MAIOR</strong>!</p>`
+    } else if (jogador > computador) {
+        res.innerHTML += `<p>Você falou ${jogador}. Meu número é <strong>MENOR</strong>!</p>`
+    } else if (jogador == computador) {
+        res.innerHTML += `<p><strong>PARABÉNS!</strong> Você acertou! Eu tinha sorteado o valor ${computador}!</p>`
+        document.getElementById('botao').style.visibility = 'hidden'
+    }
+}
