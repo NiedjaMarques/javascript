34afc285c3f09a42cb9e88c88035552db69c639f
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-04-08 16:02:46 -0300

    Create ambiente.js

---
+++ exercicios/ex029/ambiente.js
@@ -0,0 +1,29 @@
+for (var c = 1; c <= 7; c++){
+    console.log(`passo ${c}`)
+}
+
+/*
+var c = 1
+do {
+    console.log(`passo ${c}`)
+    c++
+}while (c <= 60)
+*/
+
+/*
+var c = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++ // c = c + 1 - ele era 1 vai passar a ser 2
+}
+*/
+
+/*
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+console.log('tudo bem?')
+*/
+
