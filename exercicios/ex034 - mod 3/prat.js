43e00e64212b02cf24a5e444e1102f6b3a8d0431
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-05-15 20:01:54 -0300

    Praticando

--- exercicios/ex034 - mod 3/prat.js
+++ exercicios/ex034 - mod 3/prat.js
@@ -1,7 +1,7 @@
 function iniciarContagem(){
-    let result = document.getElementById('resultado');
-    let limite = document.getElementById('inum').value;
-    result.innerHTML += `<p><strong>Contando de 0 até (numero)</strong></p>`
+    let result = document.getElementById('resultado');   
+    let limite = document.getElementById('inum').value; 
+    result.innerHTML += `<p><strong>Contando de 0 até ${limite}</strong></p>`
     document.getElementById('inum').value = "";
 
     let num = 0
@@ -10,5 +10,21 @@ function iniciarContagem(){
         num++
     }
     result.innerHTML += `&nbsp;🏁&nbsp;`;
+    
 }
+
+/*let popup = document.getElementById('popup-id');
+let input = document.getElementById('inum');
+
+input.addEventListener('change', function (event){
+
+If (validation(event) != true){
+Popup.style.display = 'block';
+}
+
+});
+Function validation(event){
+    // Aqui estará sua lógica de validação 
+}*/
+
 
