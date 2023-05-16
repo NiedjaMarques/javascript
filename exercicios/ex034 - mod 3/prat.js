21a2ac56a43bce6f8f541c56cf4b5d001272dde9
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-05-15 21:14:31 -0300

    Consertando a contagem

--- exercicios/ex034 - mod 3/prat.js
+++ exercicios/ex034 - mod 3/prat.js
@@ -1,30 +1,13 @@
 function iniciarContagem(){
     let result = document.getElementById('resultado');   
-    let limite = document.getElementById('inum').value; 
-    result.innerHTML += `<p><strong>Contando de 0 até ${limite}</strong></p>`
-    document.getElementById('inum').value = "";
-
+    let limite = document.getElementById('inum').value;
     let num = 0
-    while(num <= limite) {
-        result.innerHTML += `&nbsp;👉&nbsp; ${num}`;
-        num++
+    if (limite > 0 && (limite % 1 === 0)){
+        result.innerHTML += `<p><strong>Contando de 0 até ${limite}</strong></p>`
+        while(num <= limite) {
+            result.innerHTML += `&nbsp;👉&nbsp; ${num}`;
+            num++
+        }
+        result.innerHTML += `&nbsp;🏁&nbsp;`;
     }
-    result.innerHTML += `&nbsp;🏁&nbsp;`;
-    
 }
-
-/*let popup = document.getElementById('popup-id');
-let input = document.getElementById('inum');
-
-input.addEventListener('change', function (event){
-
-If (validation(event) != true){
-Popup.style.display = 'block';
-}
-
-});
-Function validation(event){
-    // Aqui estará sua lógica de validação 
-}*/
-
-
