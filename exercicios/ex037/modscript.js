cc49dda52ba01f2900fdfaa36b2b4141487c28fa
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-07-04 18:20:17 -0300

    Complementando tabela e exercicios

--- exercicios/ex037/modscript.js
+++ exercicios/ex037/modscript.js
@@ -1,11 +1,31 @@
 function contar(){
-    let frase = document.getElementById('substitui')
-    frase.innerHTML = `Contando:`
+    let inicio = document.getElementById('txtinicio') //chamando meu input inicio
+    let fim = document.getElementById('txtfim') //chamando meu input fim
+    let passo = document.getElementById('txtpasso') //chamando meu input passo
+    let frase = document.getElementById('result')
 
-    let contando = 1
-    while (contando <= 10){
-        frase.innerHTML += `${contando}&nbsp;👉&nbsp;`
-        contando++
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) { //se o valor colocado no -inicio- não estiver preenchido, aparece mensagem de erro.
+        frase.innerHTML = 'Impossivel contar!'
+    } else {
+        frase.innerHTML = `Contando: <br>`
+        let i = Number(inicio.value) 
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo inválido! Considerando PASSO 1')
+            p = 1
+        }
+        if (i < f) {
+            // contagem crescente
+            for(let c = i; c <= f; c += p){ 
+            frase.innerHTML += ` ${c} \u{1F449} `
+        }
+        } else {
+            // contagem regressiva
+            for (let c = i; c >= f; c -= p){
+                frase.innerHTML += ` ${c} \u{1F449} `
+            }
+        }            
+        frase.innerHTML += `\u{1F3C1}`
     }
-    frase.innerHTML += `🏁`
 }
--- exercicios/ex038/modscript.js
+++ exercicios/ex038/modscript.js
@@ -1,1 +1,14 @@
-alert('Olá')
+function calcular(){
+    let num = Number(document.getElementById('txtnum').value); // let que pega o valor que o usuario colocar na caixa. 
+    let resultado = document.getElementById('result') // let que chama uma div no html // serve para mostrar a tabuada quando for executado
+    let tabuada = ''; // let tabuada vazia 
+
+    if (num == 0){
+        window.alert('Por favor, digite um número!')
+    } else{
+        for(let cont = 1; cont <= 10; cont++){
+            tabuada += `${num} x ${cont} = ${num*cont} <br/>` //Operação um valor * o outro
+        }
+    }    
+    resultado.innerHTML = tabuada; //tabuada é a operação
+}
