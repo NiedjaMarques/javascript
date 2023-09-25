18222c7689ed86eb7ea1a94277d11b96f9124c8c
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-09-25 17:22:50 -0300

    praticando JS - repetições

---
+++ aulas/praticando013/pasta1/index.js
@@ -0,0 +1,7 @@
+/*ESTRUTURA DE REPETIÇÃO DE CONTROLE (WHILE)*/
+
+let c = 1
+while(c <= 10){ //enquanto c for menor ou igual a 6... (enquanto for true)
+    console.log(`passo ${c} `)//mostra a quantidade de passos na tela ate 6...
+    c++ //c +1 ate chegar em 6
+}
---
+++ aulas/praticando013/pasta2/index02.js
@@ -0,0 +1,6 @@
+/*ESTRUTURA DE REPETIÇÃO DE CONTROLE (DO WHILE)*/
+let n = 1
+do {
+    console.log(`contando ${n}`)//mostra na tela mensagem "contando 1, 2, 3..." ate 6
+    n++ //acrescenta +1 a n ate chegar em 6 e a condição for falsa
+} while (n <= 10); //enquanto minha condição for verdade meu bloco será executado
