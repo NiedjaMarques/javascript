1a71e2f5d7a96bcbbf76d7e151282ff758155c65
NiedjaMarques <niedjamarques4rrud4@gmail.com>
2023-09-29 11:32:39 -0300

    .

---
+++ aulas/praticando015/index.js
@@ -0,0 +1,37 @@
+/* VARIAVEIS COMPOSTAS */
+let num = [5, 8, 2, 9, 3]
+let pos = num.indexOf(9)
+console.log(`O valor 9 esta na posição ${pos}`)
+
+
+// num.push(1)
+// num.sort() //vetor colocado em ordem
+// console.log(num) //chama o vetor depois de colocado em ordem
+// console.log(`O vetor tem ${num.length} posições`)
+// console.log(`O primeiro valor do vetor é ${num[0]}`) //quem é o num na posição zero? o 5
+
+// num[3] = 6 //estou dizendo que será adicionado o valor 6 no meu indice 3
+// num.push(7) //metodo que acrescenta valores a uma array
+// num.length //atributo que me diz qual é o cumprimento do meu array
+// num.sort() //método que organiza os valores e coloca-os em ordem crescente
+
+// for (let pos = 0; pos < num.length; pos++) {
+//     console.log(`A posição ${pos} tem o valor ${num[pos]}`)//num[pos] = num no indice pos
+// } 
+
+/*A posição 0 tem o valor 1
+A posição 1 tem o valor 2
+A posição 2 tem o valor 3
+A posição 3 tem o valor 5
+A posição 4 tem o valor 8
+A posição 5 tem o valor 9*/
+
+// for (let pos in num) { //para cada posição dentro de num
+//     console.log(num[pos]) 
+// } //so funciona pra Array e objetos
+
+
+
+
+
+
